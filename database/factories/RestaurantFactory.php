4d067908e0dd0c69cc0b8b38258d33878b4fefe2
Mohammad Imran <imran17507@gmail.com>
2024-03-30 13:31:36 +0600

    Remove redundant new lines.

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -21,18 +21,15 @@ class RestaurantFactory extends Factory
 
         return [
             'title' => fake()->company(),
-            'email_address' => fake()->unique()->email(),
+            'email_address' => fake()->unique()->companyEmail(),
             'contact_no' => fake()->unique()->phoneNumber(),
             'address' => fake()->address(),
-
             "latitude" => fake()->latitude($min = ($mirpur10Latitude - (rand(0,20) / 1000)), $max = ($mirpur10Latitude + (rand(0,20) / 1000))),
             "longitude" => fake()->longitude($min = ($mirpur10Longitude - (rand(0,20) / 1000)), $max = ($mirpur10Longitude + (rand(0,20) / 1000))),
-
             'owners_name' => fake()->name(),
             'owners_email_address' => fake()->unique()->email(),
             'owners_contact_no' => fake()->unique()->phoneNumber(),
             'owners_present_address' => fake()->address(),
-
             'created_by' => 'System'
         ];
     }
