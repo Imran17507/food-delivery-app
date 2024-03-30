67d3b62923dc2b782fbc4aa93689990f4fb51786
Mohammad Imran <imran17507@gmail.com>
2024-03-30 12:49:37 +0600

    Complete rider seeding

--- app/Models/Rider.php
+++ app/Models/Rider.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
+use Database\Factories\RiderFactory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Rider extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'riders';
+    protected $primaryKey = 'id';
+    protected $guarded = ['id'];
+
+    protected static function newFactory(): Factory
+    {
+        return RiderFactory::new();
+    }
 }
--- database/factories/RiderFactory.php
+++ database/factories/RiderFactory.php
@@ -17,7 +17,17 @@ class RiderFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'email_address' => fake()->unique()->email(),
+            'contact_no' => fake()->unique()->phoneNumber(),
+            'present_address' => fake()->address(),
+            'permanent_address' => fake()->address(),
+            'nid_no' => fake()->unique()->numberBetween(1000000000, 9999999999999),
+            'passport_no' => fake()->unique()->numberBetween(1000000000, 9999999999999),
+            'emergency_contact_persons_name' => fake()->unique()->name(),
+            'emergency_contact_persons_contact_no' => fake()->unique()->phoneNumber(),
+            'created_by' => 'System'
         ];
     }
 }
