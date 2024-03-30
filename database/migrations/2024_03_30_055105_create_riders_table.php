9fd531e8b6961fc0593ef3278be1f4350f6c0a60
Mohammad Imran <imran17507@gmail.com>
2024-03-30 12:22:33 +0600

    Create riders table migration file

---
+++ database/migrations/2024_03_30_055105_create_riders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('riders', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name', 16);
+            $table->string('last_name', 16)->nullable();
+
+            $table->string('email_address', 64)->unique();
+            $table->string('contact_no', 16)->unique();
+            $table->text('present_address');
+            $table->text('permanent_address');
+
+            $table->string('nid_no', 32);
+            $table->string('passport_no', 32)->nullable();
+
+            $table->string('emergency_contact_persons_name', 32);
+            $table->string('emergency_contact_persons_contact_no', 32);
+
+            $table->timestamps();
+            $table->string('created_by', 32)->nullable();
+            $table->string('updated_by', 32)->nullable();
+            $table->softDeletes();
+            $table->string('deleted_by', 32)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('riders');
+    }
+};
