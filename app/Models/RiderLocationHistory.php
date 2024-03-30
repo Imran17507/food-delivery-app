96d801e01bea059722a27e056a20e5461ac0065a
Mohammad Imran <imran17507@gmail.com>
2024-03-30 14:41:06 +0600

    Generate model, factory and seeder for rider location history

---
+++ app/Models/RiderLocationHistory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RiderLocationHistory extends Model
+{
+    use HasFactory;
+}
