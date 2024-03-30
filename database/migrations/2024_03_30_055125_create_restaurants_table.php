4bb44962a88f65903331a1ce7595e103402d0d59
Mohammad Imran <imran17507@gmail.com>
2024-03-30 12:23:00 +0600

    Create restaurants table migration file

---
+++ database/migrations/2024_03_30_055125_create_restaurants_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title', 16);
+            $table->string('email_address', 64)->unique();
+            $table->string('contact_no', 16)->unique();
+            $table->text('address');
+
+            $table->decimal('latitude', 10, 6);
+            $table->decimal('longitude', 10, 6);
+
+            $table->string('owners_name', 32)->nullable();
+            $table->string('owners_email_address', 64)->unique();
+            $table->string('owners_contact_no', 16);
+            $table->text('owners_present_address');
+
+            $table->timestamps();
+            $table->string('created_by', 32)->nullable();
+            $table->string('updated_by', 32)->nullable();
+            $table->softDeletes();
+            $table->string('deleted_by', 32)->nullable();
+
+            $table->index(['latitude', 'longitude']);
+            $table->index('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
